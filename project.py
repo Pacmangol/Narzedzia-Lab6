4b33dc988bf4cedea1026766ca13b6b2506dcb61
Grzegorz Guz <dsw54400@dsw.edu.pl>
2024-07-10 19:18:53 +0200

    dodana funkcja zapisywania danych do json

--- project.py
+++ project.py
@@ -12,11 +12,26 @@ def load_json(file_path):
         with open(file_path, 'r') as file:
             data = json.load(file)
             print("JSON data:", data)
+            return data
     except json.JSONDecodeError:
         print("Invalid JSON format.")
+        return None
     except FileNotFoundError:
         print("File not found.")
+        return None
+
+def save_json(data, file_path):
+    if data is None:
+        print("No data to save.")
+        return
+    try:
+        with open(file_path, 'w') as file:
+            json.dump(data, file, indent=4)
+            print(f"Data saved to {file_path}")
+    except Exception as e:
+        print(f"Failed to save data: {e}")
 
 if __name__ == "__main__":
     args = parse_arguments()
-    load_json(args.source)
+    data = load_json(args.source)
+    save_json(data, args.destination)
